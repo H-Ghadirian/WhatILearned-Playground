3eb2477e43c59fcf5a68c3cc2c82197b76b26d1d
Hamed Ghadirian <h.ghadirian@outlook.com>
2023-08-16 11:36:07 +0200

    add new extension to JSONDecoder.DateDecodingStrategy

--- Pages/DateFormatter+iso8601Full.xcplaygroundpage/Contents.swift
+++ Pages/DateFormatter+iso8601Full.xcplaygroundpage/Contents.swift
@@ -1,33 +1,44 @@
 import UIKit
 
-let dateString = "2023-06-29T14:41:56.35892Z"
+//let dateString = "2023-06-29T14:41:56.35892Z"
 let jsonStr = """
 {
     "date": "2023-06-29T14:41:56.35892Z"
 }
 """
-
+//"2023-08-10T11:19:37.459Z"
 struct Root: Decodable {
     let date: Date
 }
 
 let jsonData = Data(jsonStr.utf8)
 let decoder = JSONDecoder()
-decoder.dateDecodingStrategy = .iso8601
 
+// --- 1
+decoder.dateDecodingStrategy = .iso8601
 do {
     let date = try decoder.decode(Root.self, from: jsonData)
-    print(date)
+    print("Succeed: \(date)")
 } catch {
-    print(error.localizedDescription)
+    print("Failed: \(error.localizedDescription)")
 }
 
+// --- 2
+decoder.dateDecodingStrategy = .customISO8601Strategy
+do {
+    let date = try decoder.decode(Root.self, from: jsonData)
+    print("Succeed: \(date)")
+} catch {
+    print("Failed: \(error.localizedDescription)")
+}
+
+// --- 3
 decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
 do {
     let date = try decoder.decode(Root.self, from: jsonData)
-    print(date)
+    print("Succeed: \(date)")
 } catch {
-    print(error.localizedDescription)
+    print("Failed: \(error.localizedDescription)")
 }
 
 extension DateFormatter {
@@ -39,4 +50,36 @@ extension DateFormatter {
       formatter.locale = Locale(identifier: "de_DE")
       return formatter
     }()
+}
+
+extension JSONDecoder.DateDecodingStrategy {
+    static let customISO8601Strategy = custom { decoder -> Date in
+        let dateString = try decoder.singleValueContainer().decode(String.self)
+
+        let formatterOptions: [ISO8601DateFormatter.Options] = [
+            [
+                .withFractionalSeconds,
+                .withInternetDateTime,
+                .withDashSeparatorInDate,
+                .withColonSeparatorInTime
+            ],
+            [
+                .withInternetDateTime,
+                .withDashSeparatorInDate,
+                .withColonSeparatorInTime
+            ]
+        ]
+        let formatter = ISO8601DateFormatter()
+        for formatOption in formatterOptions {
+            formatter.formatOptions = formatOption
+            if let date = formatter.date(from: dateString) {
+                return date
+            }
+        }
+        throw DecodingError.dataCorrupted(
+            DecodingError.Context(
+                codingPath: decoder.codingPath, debugDescription: "Date string not matching required format"
+            )
+        )
+    }
 }

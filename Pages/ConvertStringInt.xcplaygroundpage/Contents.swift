dc44a829f5d1fe6084892fb23a43ead5f55230b9
Hamed Ghadirian <h.ghadirian@outlook.com>
2023-09-06 03:23:27 +0200

    update Convert String to Int to String

--- Pages/ConvertStringInt.xcplaygroundpage/Contents.swift
+++ Pages/ConvertStringInt.xcplaygroundpage/Contents.swift
@@ -2,11 +2,17 @@
 
 import Foundation
 
-var number = "3.2"
-if let numberInt = Int(number) {
-    print(numberInt)
-} else {
-    print("failed")
+//var number = "3.2"
+var number = "32"
+print(update(numberOfLikes: number) ?? "failed")
+
+private func update(numberOfLikes: String) -> String? {
+    var likes = Int(numberOfLikes)
+    likes? += 1
+    if let likes = likes {
+        return String(likes)
+    }
+    return nil
 }
 
 //: [Next](@next)

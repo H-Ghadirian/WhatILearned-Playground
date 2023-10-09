06fd937f48602312c00c90b04647f10decfaf642
Hamed Ghadirian <h.ghadirian@outlook.com>
2023-10-09 13:16:31 +0200

    prefix of array

---
+++ Pages/array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var testArry = [1,2,3,4,5,6,7,8,9,10]
+let thePrefix = testArry.prefix(5)
+
+print(thePrefix)
+
+let zeroPrefix = testArry.prefix(0)
+print(zeroPrefix)
+//: [Next](@next)

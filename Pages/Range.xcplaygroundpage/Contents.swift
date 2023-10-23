66b2f9402ca645c5d9e33acf85daafd4c029514c
Hamed Ghadirian <h.ghadirian@outlook.com>
2023-10-23 11:16:25 +0200

    play with range

---
+++ Pages/Range.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var range = 1...10
+print(range)
+range.forEach { number in
+    print(number)
+}
+print("\n")
+var anotherRange = 1..<10
+anotherRange.forEach { number in
+    print(number)
+}
+//: [Next](@next)

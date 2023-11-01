adba770bb8f11f7cf00abce282f08c18d50a5541
Hamed Ghadirian <h.ghadirian@outlook.com>
2023-11-01 18:34:44 +0100

    refactor again

--- Pages/Dictionary+data.xcplaygroundpage/Contents.swift
+++ Pages/Dictionary+data.xcplaygroundpage/Contents.swift
@@ -2,19 +2,9 @@
 
 import Foundation
 
-extension Dictionary {
-    var data: Data {
-        try! JSONSerialization.data(withJSONObject: self, options: .sortedKeys)
-    }
-}
+var personDic = ["name": "Tom"]
 
-var personDic = [
-    "name": "Tom"
-]
-
-var dogDic = [
-    "name": "Tom"
-]
+var dogDic = ["name": "Tom"]
 
 if personDic.data == dogDic.data {
     print("They are the same!")

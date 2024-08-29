d4d6f4761d0127330bccb612581f563cc4430810
Hamed Ghadirian <h.ghadirian@outlook.com>
2024-08-29 20:32:39 +0200

    interesting

---
+++ Pages/Interesting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var testOne = 1
+var testTwo = 2
+var testThree = 3
+
+let testFour = testTwo = testThree
+
+print(testOne)
+print(testTwo)
+print(testThree)
+print(testFour)
